fa156e5224df1769fa2e914d9b2ac1a068f2c8dd
FrensysThomas87 <delta873@gmail.com>
2021-02-09 16:55:01 +0100

    Ultime Modifiche

--- js/script.js
+++ js/script.js
@@ -11,19 +11,19 @@ new Vue({
    //Array filtrato dal catalogo dei film che contiene solo quello che voglio visualizzare
    filteredFilm:[],
 
-   
+
+
  },
 
  methods:{
    // Funzione che fa la chiamata api al click ed esegue la funzione searchFilter()
    apiCall:function(){
      const self = this;
+
      axios.get('https://api.themoviedb.org/3/search/movie?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
      .then(function(resp){
        self.films = resp.data.results;
-
        self.searchFilter();
-
        self.searchBar = '';
 
      })
@@ -39,12 +39,15 @@ new Vue({
            originalLanguage:element.original_language,
            vote:element.vote_average
          }
-
        );
      });
+   },
 
-   }
-
-  },
+   // pageReset:function(){
+   //   return this.filteredFilm.filter((element)=>{
+   //     return this.filteredFilm === this.
+   //   })
+   // }
+ },
 });
 Vue.config.devtools = true;
