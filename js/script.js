8169c621738a4c957aad873f7bbe1300da561f39
FrensysThomas87 <delta873@gmail.com>
2021-02-09 16:37:48 +0100

    Customizzata searchbar

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ new Vue({
 
    //Array filtrato dal catalogo dei film che contiene solo quello che voglio visualizzare
    filteredFilm:[],
+
+   
  },
 
  methods:{
@@ -19,8 +21,11 @@ new Vue({
      axios.get('https://api.themoviedb.org/3/search/movie?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
      .then(function(resp){
        self.films = resp.data.results;
+
        self.searchFilter();
+
        self.searchBar = '';
+
      })
    },
 
@@ -34,6 +39,7 @@ new Vue({
            originalLanguage:element.original_language,
            vote:element.vote_average
          }
+
        );
      });
 
