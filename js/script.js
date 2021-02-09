cde6352e2a55bed212796fa5ecdc8d78b30b2df8
FrensysThomas87 <delta873@gmail.com>
2021-02-09 12:35:32 +0100

    Aggiunti tutti i parametri di ricerca e stampati a video

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ new Vue({
  data:{
    searchBar: '',
    films:[],
+   filteredFilm:[],
  },
 
  methods:{
@@ -11,16 +12,21 @@ new Vue({
      axios.get('https://api.themoviedb.org/3/search/movie?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
      .then(function(resp){
        self.films = resp.data.results;
-       console.log(self.films);
+
+       self.searchFilter();
+       console.log(self.filteredFilm);
      })
    },
 
-   // searchFilter:function(){
-   //   self.films.forEach((element) => {
-   //
-   //   });
-   //
-   // }
+   searchFilter:function(){
+     this.films.forEach((element) => {
+       this.filteredFilm.push({title:element.title,
+         originalTitle:element.original_title,
+         originalLanguage:element.original_language,
+         vote:element.vote_average});
+     });
+
+   }
 
   },
 });
