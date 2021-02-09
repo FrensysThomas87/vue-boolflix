8cab667d27f9d0e3306392f8585c3a9be6179e1c
FrensysThomas87 <delta873@gmail.com>
2021-02-09 12:09:48 +0100

    Creato metodo che fa la chiamata api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 new Vue({
  el: '#app',
  data:{
-
+   searchBar: '',
+   films:[],
  },
+
+ methods:{
+   apiCall:function(){
+     const self = this;
+     axios.get('https://api.themoviedb.org/3/search/movie?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
+     .then(function(resp){
+       self.films = resp.data.results;
+       console.log(self.films);
+     })
+   },
+
+   // searchFilter:function(){
+   //   self.films.forEach((element) => {
+   //
+   //   });
+   //
+   // }
+
+  },
 });
 Vue.config.devtools = true;
