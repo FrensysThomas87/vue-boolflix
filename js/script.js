4c191df7b5a6e76d0cb14005b0d50e20f78094e3
FrensysThomas87 <delta873@gmail.com>
2021-02-10 17:42:48 +0100

    Easeguito refactoring su milestone 1 e parte della 2

--- js/script.js
+++ js/script.js
@@ -8,18 +8,7 @@ new Vue({
    // Array che contiene tutto il catalogo dei film
    films:[],
 
-   //Array filtrato dal catalogo dei film che contiene solo quello che voglio visualizzare
-   filteredFilm:[],
-
-
-
-   lightStars:['far fa-star'],
-
-   fullStars: ['fas fa-star'],
-
-   stars:5,
-
- },
+},
 
  methods:{
    // Funzione che fa la chiamata api al click ed esegue la funzione searchFilter()
@@ -31,41 +20,20 @@ new Vue({
 
        self.films = resp.data.results;
 
-       self.searchFilter();
+
        // self.printStars();
 
        self.searchBar = '';
-       console.log(self.filteredFilm);
+       console.log(self.films);
 
      })
    },
 
-   // Crea un array con solo le proprietà che mi servono dell'array principale
-   searchFilter:function(){
-     this.films.forEach((element) => {
-       const voteParsed = parseInt(element.vote_average / 2);
-       this.filteredFilm.push(
-         {
-           title:element.title,
-           originalTitle:element.original_title,
-           originalLanguage:element.original_language,
-           vote:voteParsed,
+   parseVote:function(voto){
 
-         }
-       );
-     });
+     return parseInt(voto / 2);
    },
 
-   // printStars:function(){
-   //   this.films.forEach((element) => {
-   //     if(element.vote_average > 0){
-   //       this.fullStars.length = element.vote_average;
-   //     }else{
-   //       this.emptyStars.length = element.vote_average;
-   //     }
-   //   });
-   //
-   // },
 
 
 
