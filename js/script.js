2c113d3beb4e2c03b5f22dd3af5394fedc4bb1d6
FrensysThomas87 <delta873@gmail.com>
2021-02-10 18:00:16 +0100

    Creata funzione che mi recupera le serie tv

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ new Vue({
 
    // Array che contiene tutto il catalogo dei film
    films:[],
+   tvShows:[],
 
 },
 
@@ -17,25 +18,27 @@ new Vue({
 
      axios.get('https://api.themoviedb.org/3/search/movie?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
      .then(function(resp){
-
        self.films = resp.data.results;
-
-
-       // self.printStars();
-
        self.searchBar = '';
-       console.log(self.films);
+     });
+     
+     self.serieTvApiCall();
+   },
 
+   serieTvApiCall:function(){
+     const self = this;
+
+     axios.get('https://api.themoviedb.org/3/search/tv?api_key=427d996ca0a65b440bcbfd1d8ce45126&query= ' + this.searchBar)
+     .then(function(resp){
+       self.tvShows = resp.data.results;
+       self.searchBar = '';
+       console.log(self.tvShows);
      })
    },
 
    parseVote:function(voto){
-
      return parseInt(voto / 2);
    },
-
-
-
 
  },
 });
